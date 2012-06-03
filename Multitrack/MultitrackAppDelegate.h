716080a519f4f9371a2debccbbb1b129bc0aeb2f
Tyler <tyler@Tylers-MacBook-Pro.local>
2012-06-03 13:20:18 -0500

    Initial Commit

---
+++ Multitrack/MultitrackAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MultitrackAppDelegate.h
+//  Multitrack
+//
+//  Created by Tyler on 6/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MultitrackAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Multitrack/MultitrackViewController.h
@@ -0,0 +1,13 @@
+//
+//  MultitrackViewController.h
+//  Multitrack
+//
+//  Created by Tyler on 6/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MultitrackViewController : UIViewController
+
+@end
