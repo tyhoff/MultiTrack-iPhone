5c95af1c93680e960cf6c01d215996b57a5ed006
tyhoff <tyman71491@gmail.com>
2012-08-26 23:36:47 -0400

    updated readme

--- README.m
+++ README.m
@@ -1,1 +1,7 @@
-This is an unfinished iPhone application that can keep track of multiple stopwatches concurrently.
+MultiTrack Stopwatch
+====================
+iOS app that can keep track of multiple stopwatches in an intuitive interface.
+
+![](https://dl.dropbox.com/s/87mfpdmd92cp14k/screenshot.png?)
+
+FMMoveTable from Florian Mielke is used in this project
